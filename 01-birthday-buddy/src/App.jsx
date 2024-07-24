71eda8debd3931329629f0905924e397a1a68f9d
Bharath Devarasetty <64547981+Bharath39d@users.noreply.github.com>
2024-07-24 12:36:06 +0530

    Update App.jsx

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -21,4 +21,72 @@ function App() {
 }
 
 
+private renderSectionC = () => {
+    const { DigiGAMSectionC } = this.state;
+    this._allItemsSectionC = DigiGAMSectionC && DigiGAMSectionC.length > 0 && DigiGAMSectionC.map(item => {
+        return ({
+            SLNo: item["SL.No"],
+            PrimaryStatement: item["Primary Statement"],
+            CategoryName: item["Category Name"],
+            Group: item["Group"], 
+            Alias: item["Alias"],
+            LeadSheetThreshold: item["Lead Sheet Threshold"],
+            ProcessMapThreshold: item["Process Map Threshold"], 
+            PreparerMapThreshold: item["Preparer Map Threshold"],
+            AccountType: item["Account Type"],
+            AccountSubtype: item["Account Sub-type"],
+            AccountClass: item["Account Class"],
+            AccountSubclass: item["Account Sub-class"],
+            GLAccounts: item["GL Accounts"],
+            AccountClassALL: item["Account Class (ALL)"],
+            AccountSubclassALL: item["Account Sub-class (ALL)"],
+            GLAccountsALL: item["GL Accounts (ALL)"],
+            ColorIndex: item["Color Index"],
+            IFRS: item["IFRS"],
+            CommonSelectionThresholds: item["Common Selection Thresholds"], 
+            MultipleSubmission: item["Multiple Submission"],
+            ActivityAnalysisAboveThreshold: item["Activity Analysis Above Threshold"], 
+            QualitiveFactors: item["Qualitive Factors"],
+            AccountTypeALL: item["Account Type (ALL)"],
+            AccountSubtypeALL: item["Account Subtype (ALL)"],
+            ImportGroupName: item["ImportGroupName"],
+            ServerGroupName: item["ServerGroupName"],
+            Testing: item["Testing"],
+            OSJESelection: item["OSJESelection"], 
+            S2C: item["S2C"],
+            CheckboxValue: item["CheckboxValue"], 
+            AnalysisSelection: item["AnalysisSelection"],
+            Analysis: item["Analysis"]
+        });
+    });
+
+    console.log("this._allItemsSectionC >> " + JSON.stringify(this._allItemsSectionC));
+
+    return (
+        <React.Fragment>
+            <div className={styles.detailListContainer}>
+                <DetailsList
+                    items={this._allItemsSectionC}
+                    columns={this._columnsSectionC}
+                    setKey="none"
+                    layoutMode={DetailsListLayoutMode.fixedColumns}
+                    selectionMode={SelectionMode.none}
+                    isHeaderVisible={true}
+                    //onItemInvoked={this._onItemInvoked}
+                    compact={false}
+                    onRenderDetailsHeader={this._onRenderHeaderRow}
+                    styles={{
+                        root: {
+                            backgroundColor: '#2e2e2e !important'
+                        },
+                        focusZone: {
+                            backgroundColor: '#2e2e2e !important'
+                        }
+                    }}
+                />
+            </div>
+        </React.Fragment>
+    );
+}
+
 	
