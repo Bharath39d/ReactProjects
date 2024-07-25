bf6f2b323f58d450cf9e38056d67d275502d139b
Bharath Devarasetty <64547981+Bharath39d@users.noreply.github.com>
2024-07-25 12:09:04 +0530

    Update App.jsx

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -20,27 +20,3 @@ function App() {
   );
 }
 
- private async updateHistorySharePointList(data: any[]) {
-    const listTitle = "History";
-    const list = sp.web.lists.getByTitle(listTitle);
-    const batch = sp.web.createBatch();
-    const fieldNames = await this.getSharePointListFieldNames(list);
-
-    for (const item of data) {
-      try {
-        const formattedItem = this.formatItemForSharePoint(item, fieldNames);
-        const existingItem = await list.items.filter(`ID eq ${item.ID}`).get();
-          list.items.getById(item.ID).inBatch(batch).update(formattedItem)
-            .catch((error) => {
-              console.error('Error updating item in SharePoint list', error.message);
-              console.error('Error details:', JSON.stringify(error));
-            });
-        }
-      catch (error) {
-        console.error('Error preparing item for SharePoint list', error.message);
-        console.error('Error details:', JSON.stringify(error));
-      }
-    }
-
-    await batch.execute();
-  }
